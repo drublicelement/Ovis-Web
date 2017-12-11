157dd7a93da1286f7dcd96fe11ee0b2f1f879548
drublicelement <pcasabonaore@gmail.com>
2017-12-11 17:42:52 -0500

    ovis final 11-12-2017

--- enviar.php
+++ enviar.php
@@ -1,14 +1,19 @@
 <?php
-$destino = "pcasabonaore@gmail.com";
+
+$destino = "contacto@ovis.pe, pcasabonaore@gmail.com";
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $telefono = $_POST["telefono"];
 $empresa = $_POST["empresa"];
 $consulta = $_POST["consulta"];
-$contenido = "Nombre: " . $nombre . "\nE-mail: " . $email . "\nTelefono: " . $telefono . "\nEmpresa: " . $empresa . "\nConsulta: " . $consulta;
-$asunto = "Contacto desde la Web de Ovis Communication",
+
+
+$contenido = "Nombre: " . $nombre . "\nE-mail: " . $email . "\nTelefono: " . $telefono . "\nEmpresa: " . 
+$empresa . "\nConsulta: " . $consulta;
+$asunto = "Contacto desde la Web de Ovis Communication";
 mail($destino,$asunto, $contenido);
-header('Location:gracias.html')
+
+header('Location:hola.html')
 
 
 
