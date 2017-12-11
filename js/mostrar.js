2ad83dd7da6ec29539d2682139cb9f514be80749
drublicelement <pcasabonaore@gmail.com>
2017-12-11 12:30:42 -0500

    subiendo el resposive modificado

--- js/mostrar.js
+++ js/mostrar.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
    $('#boton-menu').on("click", function() {        
       $("#menu-principal").toggle(500);
+      $(".st0").addClass("st4");
+      $(".st1").addClass("st5");
     });
 
-
+ 
      $('#menu-principal').on("click", function() {        
       $("#menu-principal").toggle(500);
+      $(".st0").removeClass("st4");
+      $(".st1").removeClass("st5");
     });
 
 });
