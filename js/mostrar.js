fde43a84acb04f5b6d15007e4e3b855fb33e3a36
drublicelement <pcasabonaore@gmail.com>
2017-12-10 23:12:30 -0500

    mejorando flechas prev y next de sliders

--- js/mostrar.js
+++ js/mostrar.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
    $('#boton-menu').on("click", function() {        
       $("#menu-principal").toggle(500);
     });
+
+
+     $('#menu-principal').on("click", function() {        
+      $("#menu-principal").toggle(500);
+    });
+
 });
+
+
+
+
