2ecde76e03683288fb2714fc8854aee40db30b24
drublicelement <pcasabonaore@gmail.com>
2017-12-11 15:31:16 -0500

    formulario

--- enviar.php
+++ enviar.php
@@ -6,9 +6,9 @@ $telefono = $_POST["telefono"];
 $empresa = $_POST["empresa"];
 $consulta = $_POST["consulta"];
 $contenido = "Nombre: " . $nombre . "\nE-mail: " . $email . "\nTelefono: " . $telefono . "\nEmpresa: " . $empresa . "\nConsulta: " . $consulta;
-$asunto = "Contacto desde la Web de Ovis Communication"
+$asunto = "Contacto desde la Web de Ovis Communication",
 mail($destino,$asunto, $contenido);
-header("Location:gracias.html")
+header('Location:gracias.html')
 
 
 
